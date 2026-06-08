a539883c878d271a11b478975ba5ea22d78111e7
bob <leificonz@gmail.com@gmail.com>
2026-06-07 23:29:38 -0400

    s3.tf

---
+++ s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "test-bucket-993564826887"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+# resource "aws_s3_bucket" "bobbucket" {
+# bucket = "bob-versioning"
+# }
+
+resource "aws_s3_bucket" "bobby" {
+bucket = "test-sse3"
+}
